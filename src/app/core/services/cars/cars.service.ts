8b9a22487f6215c1dcec8444dcb4a679385ce476
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2023-03-29 23:15:04 +0200

    input filter ok

--- src/app/core/services/cars/cars.service.ts
+++ src/app/core/services/cars/cars.service.ts
@@ -21,4 +21,5 @@ export class CarsService {
       body
     );
   }
+ 
 }
--- src/app/pages/listado/listado.component.ts
+++ src/app/pages/listado/listado.component.ts
@@ -1,6 +1,7 @@
 import { ICar } from './../../core/services/models/cars-models';
 import { Component } from '@angular/core';
-import { CarsService } from '../../core/services/cars/cars.service'
+import { CarsService } from '../../core/services/cars/cars.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-listado',
@@ -19,13 +20,43 @@ export class ListadoComponent {
     this.getCars();//lanzo la función al iniciar 
   }
 
-   private getCars() {
-    this.carsservice.getCars().subscribe((cars) => {
-      this.cars = cars;
-      console.log(this.cars,26);
-      
-      //this.filteredProducts = products;
-    });
+  //  private getCars() {
+  //   this.carsservice.getCars().subscribe((cars) => {
+  //     this.cars = cars;
+  //   });
+  // }
+  
+  // capturarTexto(event: any) {
+  //   let textoDigitado = event.target.value;
+  //   console.log(textoDigitado);
+  // }
+
+  // 
+  private getCars(textoDigitado?: string) {
+    if (textoDigitado) {
+      this.carsservice.getCars().pipe(
+        map(cars => {
+          console.log(cars,38); // log the cars to the console
+          //return cars.filter(car => car.marca.toLowerCase() === textoDigitado.toLowerCase())
+          return cars.filter(car => car.marca.toLowerCase().includes(textoDigitado.toLowerCase()))
+        })
+      ).subscribe(filteredCars => {
+        this.cars = filteredCars;
+      });
+    } else {
+      this.carsservice.getCars().subscribe(cars => {
+        console.log(cars,46); // log the cars to the console
+        this.cars = cars;
+      });
+    }
   }
+  
+  capturarTexto(event: any) {
+    let textoDigitado = event.target.value;
+    console.log(textoDigitado,5445454);
+    this.getCars(textoDigitado);
+  }
+  
+  
   
 }
