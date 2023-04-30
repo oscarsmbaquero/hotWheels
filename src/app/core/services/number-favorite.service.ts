e18ccaacb0181ae4979904465cdef1efdfef1caf
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2023-04-30 20:00:08 +0200

    test favorite

--- src/app/core/services/number-favorite.service.ts
+++ src/app/core/services/number-favorite.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { CarsService } from './cars/cars.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoriteCarsCountService {
   private _favoriteCarsCount: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+
+  constructor(private carsService: CarsService) {
+    this.carsService.getFavoriteCars().subscribe(favoriteCars => {
+      this._favoriteCarsCount.next(favoriteCars.length);
+    });
+  }
 
   updateFavoriteCarsCount(count: number) {
     console.log(count, 11)
